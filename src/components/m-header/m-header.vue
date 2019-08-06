4df32288e873b6b109e7f74497d8706afcd103aa
lxh <1064971176@qq.com>
2019-08-06 09:52:08 +0800

    修改

--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -2,7 +2,7 @@
   <div class="m-header">
     <div class="icon"></div>
     <h1 class="text"></h1>
-    <!-- <h1 class="text">Music Box</h1> -->
+    <h1 class="text">U·L Music</h1>
     <router-link tag="div" class="mine" to="/user">
       <i class="icon-mine"></i>
     </router-link>
@@ -10,40 +10,45 @@
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+export default {};
 </script>
 
 <style scoped lang="scss">
-  @import "@/assets/css/variable.scss";
-  @import "@/assets/css/mixin.scss";
+@import "@/assets/css/variable.scss";
+@import "@/assets/css/mixin.scss";
 
-  .m-header{
-    position: relative;
-    height: 44px;
-    text-align: center;
-    color: $color-theme;
-    font-size: 0;
-    .icon{
-      display: inline-block;
-      vertical-align: top;
-      margin-top: 6px;
-      width: 30px;
-      height: 32px;
-      margin-right: 9px;
-      @include bg-image('logo');
-      background-size: 30px 32px;}
-    .text{
-      display: inline-block;
-      vertical-align: top;
-      line-height: 44px;
-      font-size: $font-size-large;}
-    .mine{
-      position: absolute;
-      top: 0;
-      right: 0;
-      .icon-mine{
-        display: block;
-        padding: 12px;
-        font-size: 20px;
-        color: $color-theme}}}
+.m-header {
+  position: relative;
+  height: 44px;
+  text-align: center;
+  color: $color-theme;
+  font-size: 0;
+  .icon {
+    display: inline-block;
+    vertical-align: top;
+    margin-top: 6px;
+    width: 30px;
+    height: 32px;
+    margin-right: 9px;
+    @include bg-image("logo");
+    background-size: 30px 32px;
+  }
+  .text {
+    display: inline-block;
+    vertical-align: top;
+    line-height: 44px;
+    font-size: $font-size-large;
+  }
+  .mine {
+    position: absolute;
+    top: 0;
+    right: 0;
+    .icon-mine {
+      display: block;
+      padding: 12px;
+      font-size: 20px;
+      color: $color-theme;
+    }
+  }
+}
 </style>
